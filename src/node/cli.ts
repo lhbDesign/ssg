2bd528816fbc28418eb3492893bccc62eb1937de
lihaibo <gjzxlihaibo@vip.qq.com>
2022-11-26 17:43:27 +0800

    CLI脚手架和Dev Server 搭建

---
+++ src/node/cli.ts
@@ -0,0 +1,34 @@
+import { cac } from "cac";
+import path = require("path");
+
+const version = require("../../package.json").version;
+
+const cli = cac("island").version(version).help();
+
+import { createDevServer } from "./dev"; 
+
+cli
+  .command("[root]", "start dev server")
+  .alias("dev")
+  .action(async (root: string) => {
+    // 添加以下逻辑
+    root = root ? path.resolve(root) : process.cwd();
+    const server = await createDevServer(root);
+    await server.listen();
+    server.printUrls();
+  });
+  
+// cli
+//   .command("[root]", "start dev server")
+//   .alias("dev")
+//   .action(async (root: string) => {
+//     console.log("dev", root);
+//   });
+
+cli
+  .command("build [root]", "build for production")
+  .action(async (root: string) => {
+    console.log("build", root);
+  });
+
+cli.parse();
